f1004a9c0b984046d396676019ae5bfb64598156
abie_x <abhiramzmenon@gmail.com>
2023-12-23 00:10:29 +0530

    testing..

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -43,7 +43,7 @@ async function buildPdf(name, course, batch, phoneNumber, email, intake, admissi
     
             doc.fontSize(16);
     
-            doc.image('./images/test.png', 250, 0, {fit: [100, 100], align: 'center' })
+            doc.image('/test.png', 250, 0, {fit: [100, 100], align: 'center' })
     
             
     
